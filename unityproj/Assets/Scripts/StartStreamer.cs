c8ce22cef85730307a1c1cab8076cfa372d52b34
nir6760 <58267005+nir6760@users.noreply.github.com>
2021-10-08 11:19:39 +0300

    unity proj first commit

---
+++ unityproj/Assets/Scripts/StartStreamer.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class StartStreamer : MonoBehaviour
+{
+    
+
+#if ENABLE_WINMD_SUPPORT
+        
+        [DllImport("HL2RmStreamUnityPlugin", EntryPoint = "StartStreaming", CallingConvention = CallingConvention.StdCall)]
+        public static extern void StartDll();
+#endif
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        Debug.Log("Windows Runtime Support enabled 0 ");
+#if ENABLE_WINMD_SUPPORT
+    Debug.Log("Windows Runtime Support enabled 3");
+                    StartDll();
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
